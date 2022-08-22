eca1ee2952a5596469f3cbf65ab1d85bb4ece9f6
scottswitzer <55900643+scottswitzer@users.noreply.github.com>
2022-08-22 11:46:06 -0700

    General code clean up
    Clean up of code and added a couple notes

--- rs_save.cpp
+++ rs_save.cpp
@@ -1,10 +1,9 @@
-//scott test edit 2.0
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 
 #include <fstream>              // File IO
 #include <iostream>             // Terminal IO
 #include <sstream>              // Stringstreams
-using namespace std;
+using namespace std;            
 #include <limits>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -22,6 +21,7 @@ using namespace std;
 #include <stdexcept>
 #include <signal.h>
 
+
 //Saving firmware for FishSense prototype 2.0 (TX2)
 //February 12th, 2022
 //Peter Tueller ptueller@eng.ucsd.edu
@@ -30,7 +30,7 @@ using namespace std;
 //Depth sensor integrated into code
 
 std::string exec(const char* cmd) {
-    char buffer[128];
+    char buffer[128]; //buffer
     std::string result = "";
     FILE* pipe = popen(cmd,"r");
     if(!pipe) throw std::runtime_error("popen() failed!");
@@ -48,6 +48,7 @@ std::string exec(const char* cmd) {
 
 int main(int argc, char* argv[]) try
 {
+                
     rs2::config cfg;
     cfg.enable_stream(RS2_STREAM_DEPTH,1280,720,RS2_FORMAT_Z16,15);
     cfg.enable_stream(RS2_STREAM_COLOR,1280,720,RS2_FORMAT_RGB8,15);
@@ -78,7 +79,7 @@ int main(int argc, char* argv[]) try
     bag_location << dirprefix.str().c_str() << bag_index << ".bag";
     cfg.enable_record_to_file(bag_location.str().c_str());
 
-     //Set up GPIOs
+    //Set up GPIOs
     system("echo 298 > /sys/class/gpio/export");  //298 is the recording switch
     cout << "298 export\n"; 
     system("echo 388 > /sys/class/gpio/export");  //388 is the rec indicator light
@@ -98,16 +99,18 @@ int main(int argc, char* argv[]) try
         std::this_thread::sleep_for(std::chrono::milliseconds(150));
         cout << "blink\n";
     }
-	
-   system("echo 0 > /sys/class/gpio/gpio388/value"); //Leave indicator light on after booting
 
-	int pid = -1;
+    system("echo 0 > /sys/class/gpio/gpio388/value"); //Leave indicator light on after booting
+
+
+    int pid = -1;
     while(true) {
         if(exec("cat /sys/class/gpio/gpio298/value") == "0\n") { //If switch is turned on
             cout << "saw reed switch\n";
             rs2::pipeline pipe;
             if(!rec_flag) { //And are not already recording
-		    //Blink the REC LED to acknowledge switch has been turned on
+                
+                //Blink the REC LED to acknowledge switch has been turned on
                 for(int i=0; i<3; i++) {
                     system("echo 0 > /sys/class/gpio/gpio388/value");
                     std::this_thread::sleep_for(std::chrono::milliseconds(150));
@@ -115,10 +118,10 @@ int main(int argc, char* argv[]) try
                     std::this_thread::sleep_for(std::chrono::milliseconds(150));
                     cout << "blink\n";
                 }
-               
+                
                 rec_flag=true;
-		    
-		           //call python for depth sensor integration
+
+                //call python for depth sensor integration
                 pid = fork();
                 if (pid == 0) {
                     cout << "spawing python" << endl;
@@ -126,25 +129,26 @@ int main(int argc, char* argv[]) try
                     // I'm the child
                     return 0;
                 }
-		    
+
                 std::this_thread::sleep_for(std::chrono::milliseconds(1000));
                 pipe.start(cfg); //bag file should be saving at this point!
 		        cout << "saved bag to " << bag_location.str() << endl;
             }
 
             while(rec_flag) {
+                
                 //We are having fun and saving the bag, all is well in the world
                 //Fun is continuing to be had
-		    
-		//while recording we want a slow blink from the rec LED
+
+                //while recording we want a slow blink from the rec LED
                 system("echo 0 > /sys/class/gpio/gpio388/value");
                 std::this_thread::sleep_for(std::chrono::milliseconds(750));
                 system("echo 1 > /sys/class/gpio/gpio388/value");
                 std::this_thread::sleep_for(std::chrono::milliseconds(750));
               
-               if(exec("cat /sys/class/gpio/gpio298/value") == "1\n") { //If the rec switch is turned off
-			
-			      //Blink the REC LED to acknowledge the switch has been turned off
+                if(exec("cat /sys/class/gpio/gpio298/value") == "1\n") { //If the rec switch is turned off
+
+                    //Blink the REC LED to acknowledge the switch has been turned off
                     for(int i=0; i<3; i++) {
                         system("echo 0 > /sys/class/gpio/gpio388/value");
                         std::this_thread::sleep_for(std::chrono::milliseconds(150));
@@ -152,29 +156,28 @@ int main(int argc, char* argv[]) try
                         std::this_thread::sleep_for(std::chrono::milliseconds(150));
                         cout << "blink\n";
                     }
-			
+
                     rec_flag = false;
-		    system("echo 0 > /sys/class/gpio/gpio388/value"); //Rec LED returns to solid ON
-			
-               
+                    system("echo 0 > /sys/class/gpio/gpio388/value"); //Rec LED returns to solid ON
+
                     pipe.stop(); //This should save the bag file
-		       kill(pid, SIGTERM); //end python call
+                    kill(pid, SIGTERM); //end python call
                     pid = -1;
-               
+
                     system("sync");
                     system("sync");
                     system("sync");
-                    
+
                     system("umount /mnt/data");
-                   
+
                     system("mount /mnt/data");
-                   
+
                     //Prep the potential next bag file
                     bag_index++;
                     std::stringstream bag_location_new;
                     bag_location_new << dirprefix.str().c_str() << bag_index << ".bag";
                     cfg.enable_record_to_file(bag_location_new.str().c_str());
-                    std::this_thread::sleep_for(std::chrono::milliseconds(2000)); //Don't want to catch multiple magnetic switches
+                    std::this_thread::sleep_for(std::chrono::milliseconds(2000)); //Don't want to catch multiple rec switches
                 }
             }
         }
